8fe4a3c137346cc902549b8d5136affd7acdd29c
zakariasg <zakariasghirpc664@gmail.com>
2026-07-08 19:19:51 +0100

    Premier commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import GuestLayout from '@/Layouts/GuestLayout';
+
+export default function Home({ stats }) {
+    return (
+        <GuestLayout>
+            <div className="p-10 text-center">
+                
+            </div>
+        </GuestLayout>
+    );
+}
